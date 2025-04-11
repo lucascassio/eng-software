79c13b36b037cd94067c1eee0e390e0aaf893bc4
Lucas Cassio Costa <lccosta2604@gmail.com>
2025-04-11 07:22:14 -0300

    adding jwt token and login

--- ApiTrocaLivros/obj/Debug/net9.0/ApiTrocaLivros.AssemblyInfo.cs
+++ ApiTrocaLivros/obj/Debug/net9.0/ApiTrocaLivros.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiTrocaLivros")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b1f7a35a6f6030ac4adad1ca0289d8bfcae99a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fde63bb5a4e40e368d518e0f6d2ae6e5cad948ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiTrocaLivros")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiTrocaLivros")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
