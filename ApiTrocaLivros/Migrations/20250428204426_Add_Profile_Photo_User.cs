b6b1b3858659faab423856bb99a35332bef4fcf4
lukasara3 <lucaoamsilva@gmail.com>
2025-05-06 22:44:41 -0300

    push

---
+++ ApiTrocaLivros/Migrations/20250428204426_Add_Profile_Photo_User.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiTrocaLivros.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Profile_Photo_User : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProfilePhoto",
+                table: "users",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfilePhoto",
+                table: "users");
+        }
+    }
+}
